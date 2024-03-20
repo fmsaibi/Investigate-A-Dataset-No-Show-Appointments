43a2578b1792113855dc5ab843d6be0fa7c6215a
Fahad Al Khusaibi <fmsaibi@gmail.com>
2024-03-20 17:10:33 +0400

    initial commit

---
+++ constants/color.py
@@ -0,0 +1,15 @@
+"""
+A collection of Color used for displaying information in the program.
+
+Attributes:
+    BLUE (str)  : returns 'b'
+    GREEN (str) : returns 'g'
+    RED (str)   : returns 'r'
+    BLACK (str) : returns 'k'
+    WHITE (str) : returns 'w'
+"""
+BLUE    = 'b'
+GREEN   = 'g'
+RED     = 'r'
+BLACK   = 'k'
+WHITE   = 'w'
